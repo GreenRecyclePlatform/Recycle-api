d27c4bfdec21d52f050200efe121467d74698516
mohammed <mohammed.mechengineer.70@gmail.com>
2025-11-11 13:23:03 +0200

    step 0

---
+++ recycle.Infrastructure/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using recycle.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recycle.Infrastructure
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        //add dbsets here
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ recycle.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using recycle.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recycle.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        //add repositorys here
+        
+        public UnitOfWork(AppDbContext context
+            //add repository parameters here
+            )
+        {
+            _context = context;
+            //initialize repositories here
+            
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
