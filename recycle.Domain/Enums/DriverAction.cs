e1a989debfb5e7d6251e4ca69df8a45d3c2b3905
Aya-Elaidy <ayaelaidy0@gmail.com>
2025-11-12 18:16:02 +0200

    Add DriverAssignment entity, DTOs, and configuration (#4)
    * Add DriverAssignment entity, DTOs, and configuration
    
    * Add DriverAssignments DbSet to AppDbContext
    
    * Update DriverAssignment
    
    * Remove duplicate DriverAssignments DbSet
    
    Removed duplicate DbSet for DriverAssignments.
    
    * Change DriverAssignment properties to use Guid and string
    
    * Uncomment relationship configurations in DriverAssignment
    
    * Change RequestId and DriverId types in DTO
    
    * Change AssignmentId and RequestId to Guid types
    
    * Change AssignmentId type from int to Guid
    
    * Change AssignmentId type from int to Guid
    
    ---------
    
    Co-authored-by: Mohammed-Hassan <158804620+Mohammed-Bata@users.noreply.github.com>

---
+++ recycle.Domain/Enums/DriverAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recycle.Domain.Enums
+{
+    public enum DriverAction
+    {
+        Accept = 1,
+        Reject = 2
+    }
+}
--- recycle.Infrastructure/AppDbContext.cs
+++ recycle.Infrastructure/AppDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using recycle.Domain;
+using recycle.Infrastructure.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +18,7 @@ namespace recycle.Infrastructure
 
         }
         //add dbsets here
+        public DbSet<DriverAssignment> DriverAssignments { get; set; }
         public DbSet<Payment> Payments { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<DriverProfile> DriverProfiles { get; set; }
@@ -23,8 +26,10 @@ namespace recycle.Infrastructure
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+
             builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
             base.OnModelCreating(builder);
+
         }
     }
 }
