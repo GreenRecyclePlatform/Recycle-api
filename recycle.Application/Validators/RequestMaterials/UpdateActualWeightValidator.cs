cf40e0c2af7b873b8721ec2a71f3605e5654f05f
Madonnazakaria <madonna8zakaria@gmail.com>
2025-11-14 13:47:14 +0200

    madonnas new updates

---
+++ recycle.Application/Validators/RequestMaterials/UpdateActualWeightValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using recycle.Application.DTOs.RequestMaterials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recycle.Application.Validators.RequestMaterials
+{
+    public class UpdateActualWeightValidator : AbstractValidator<UpdateActualWeightDto>
+    {
+        public UpdateActualWeightValidator()
+        {
+            RuleFor(x => x.ActualWeight)
+                .GreaterThanOrEqualTo(0).WithMessage("Actual weight cannot be negative")
+                .LessThanOrEqualTo(10000).WithMessage("Actual weight cannot exceed 10,000 kg");
+
+            RuleFor(x => x.Notes)
+                .MaximumLength(500).WithMessage("Notes cannot exceed 500 characters")
+                .When(x => !string.IsNullOrEmpty(x.Notes));
+        }
+    }
+}
