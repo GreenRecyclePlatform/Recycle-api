f8a33a83c193020f9172dc233e8b54b7add8bd02
mohammed <mohammed.mechengineer.70@gmail.com>
2025-12-10 02:41:16 +0200

    deleteoptions

--- recycle.Infrastructure/DependencyInjection.cs
+++ recycle.Infrastructure/DependencyInjection.cs
@@ -102,7 +102,6 @@ using recycle.Application.Interfaces;
 using recycle.Application.Interfaces.IRepository;
 using recycle.Application.Interfaces.IService;
 using recycle.Application.Interfaces.IService.recycle.Application.Interfaces;
-using recycle.Application.Options;
 using recycle.Application.Services;
 using recycle.Infrastructure.ExternalServices;
 using recycle.Infrastructure.Repositories;
