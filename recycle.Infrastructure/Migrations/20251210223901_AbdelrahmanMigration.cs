823ae50caa95ed8a3802a022ab8092216e44d88a
MrHelwa <abdulrahmanibrahemhelwa@hotmail.com>
2025-12-11 00:41:34 +0200

    Some More APIs for PayPal

--- recycle.Infrastructure/Migrations/20251210223901_AbdelrahmanMigration.cs
+++ recycle.Infrastructure/Migrations/20251210223901_AbdelrahmanMigration.cs
@@ -5,25 +5,18 @@
 namespace recycle.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class RemoveIsPrimaryFromUser : Migration
+    public partial class AbdelrahmanMigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "IsPrimary",
-                table: "Addresses");
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<bool>(
-                name: "IsPrimary",
-                table: "Addresses",
-                type: "bit",
-                nullable: false,
-                defaultValue: false);
+
         }
     }
 }
